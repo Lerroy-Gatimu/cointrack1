65dbb26968b3f8ead20aa24aafe76ba27e847b20
Lerroy Gatimu <lerroygatimu@gmail.com>
2026-02-21 17:32:42 +0300

    2nd commit

--- cointrack1/settings.py
+++ cointrack1/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-40=mmk&+dnqug^psrs6#*7o$zg(bph9c5$ie8#m%$spb-em2h6"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,9 +79,17 @@ WSGI_APPLICATION = "cointrack1.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'adorn_jewellery_db',
+        'USER': 'root',  
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': '127.0.0.1', 
+        'PORT': '3306',  
+        'OPTIONS': {
+            'charset': 'utf8mb4',  # Supports emojis/special chars if needed
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",  # For compatibility with Django
+        },
     }
 }
 
